d8469d8e616378d5abdb76ff7ba6ef2211d7c5bd
3B-OD <tamshaabdullah@gmail.com>
2025-10-07 21:16:34 +0300

    updated:   3-setInterval.js and 4-server.js

--- event-loop-examples/3-setInterval.js
+++ event-loop-examples/3-setInterval.js
@@ -1,6 +1,12 @@
-setInterval(() => {
-  console.log("Hi");
+const interval = setInterval(() => {
+    console.log("Hi");
+    clearInterval(interval); 
 }, 2000);
 
 console.log("I will run first");
 
+
+
+//process stays alive unless we stop it manually (ctrl + c) or we have no more work to do
+//in this case, we have setInterval, so it will run forever every 2 seconds
+//to stop it after some time, we can use clearInterval
